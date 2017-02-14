5370dca739d223e9e105bb0865f9b29d6bd297d0
acgotaku <acgotaku311@gmail.com>
2017-02-14 13:59:00 +0800

    fix config file

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,22 +3,24 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
-module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
+module.exports = function (metadata) {
+  return webpackMerge(commonConfig(metadata), {
+      devtool: 'cheap-module-eval-source-map',
 
-  output: {
-    path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/',
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
-  },
+      output: {
+          path: helpers.root('dist'),
+          publicPath: 'http://localhost:8080/',
+          filename: '[name].js',
+          chunkFilename: '[id].chunk.js'
+      },
 
-  plugins: [
-    new ExtractTextPlugin('[name].css')
-  ],
+      plugins: [
+          new ExtractTextPlugin('[name].css')
+      ],
 
-  devServer: {
-    historyApiFallback: true,
-    stats: 'minimal'
-  }
-});
+      devServer: {
+          historyApiFallback: true,
+          stats: 'minimal'
+      }
+  });
+};
